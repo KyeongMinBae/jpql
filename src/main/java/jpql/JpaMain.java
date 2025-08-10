ec9cd8030f31a7d99c1c70cc1199f2d87a32e21c
KyeongMinBae <reaaaaaaal@gmail.com>
2025-08-10 20:10:26 +0900

    프로젝션(SELECT)

---
+++ src/main/java/jpql/JpaMain.java
@@ -0,0 +1,42 @@
+package jpql;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+public class JpaMain {
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setAge(10);
+            em.persist(member);
+
+            em.flush();
+            em.clear();
+
+            List<MemberDTO> result = em.createQuery("select new jqp.MemberDTO(m.username, m.age0 from Member m", MemberDTO.class)
+                             .getResultList();
+
+            MemberDTO memberDTO = result.get(0);
+            System.out.println("result = " + memberDTO.getUsername());
+            System.out.println("result = " + memberDTO.getAge());
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+            e.printStackTrace();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+}
